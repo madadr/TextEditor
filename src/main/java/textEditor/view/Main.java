bbfcb4d30794a3e8fae7a2eaadc928a4e5d9ad2c
mdjdrn1 <mdjdrn1@gmail.com>
2017-11-12 14:15:22 +0100

    Added textArea to main app view

--- src/main/java/textEditor/view/Main.java
+++ src/main/java/textEditor/view/Main.java
@@ -3,15 +3,18 @@ package textEditor.view;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.MenuBar;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.ToolBar;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    // TODO: Are these members required? Can they be replaced with local objects?
     private BorderPane root;
     private MenuBar menuBar;
     private ToolBar toolBar;
+    private TextArea textArea;
 
     @Override
     public void start(Stage stage) throws Exception {
@@ -21,12 +24,14 @@ public class Main extends Application {
 
         menuBar = new EditorMenuBar();
         toolBar = new EditorToolBar();
+        textArea = new EditorTextArea();
 
         VBox topBox = new VBox();
         topBox.getChildren().add(menuBar);
         topBox.getChildren().add(toolBar);
 
         root.setTop(topBox);
+        root.setCenter(textArea);
 
         stage.setTitle("Text Editor");
         stage.setScene(scene);
