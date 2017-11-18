cf823a4efd091fb26bd6dc4835cc259833a2b5f3
mdjdrn1 <mdjdrn1@gmail.com>
2017-11-18 22:22:01 +0100

    Replaced EditorScene with EditorStage

--- src/main/java/textEditor/view/MainApp.java
+++ src/main/java/textEditor/view/MainApp.java
@@ -1,33 +1,22 @@
 package textEditor.view;
 
 import javafx.application.Application;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
-    private static final int EDITOR_SCENE_WIDTH = 600;
-    private static final int EDITOR_SCENE_HEIGHT = 400;
-
     private Stage stage;
-    private Scene editorScene = new EditorScene(this, EDITOR_SCENE_WIDTH, EDITOR_SCENE_HEIGHT);
 
     @Override
     public void start(Stage stage) throws Exception {
         this.stage = stage;
 
+        stage = new EditorStage(this);
+
         stage.setTitle("Editor");
-        stage.setScene(editorScene);
-        stage.setResizable(false);
+
+        stage.setResizable(true);
 
         stage.show();
-    }
-
-    public Scene getEditorScene() {
-        return editorScene;
-    }
-
-    public void setEditorScene(Scene scene) {
-        this.stage.setScene(scene);
     }
 
     public static void main(String[] args) {
