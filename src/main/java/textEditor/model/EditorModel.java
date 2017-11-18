3d3b96144ba54f81f3a998bd135bc32cc2bc01b9
mdjdrn1 <mdjdrn1@gmail.com>
2017-11-18 23:22:14 +0100

    Partially added observer to text in EditorModel

--- src/main/java/textEditor/model/EditorModel.java
+++ src/main/java/textEditor/model/EditorModel.java
@@ -1,22 +1,41 @@
 package textEditor.model;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
 public class EditorModel {
     private String text;
-    private int position;
+
+    List<Consumer<String>> textObservers = new ArrayList<>();
+
+    private int caretPosition;
 
     public String getText() {
-        return text;
+        return text.toString();
     }
 
     public void setText(String text) {
         this.text = text;
+        textObservers.forEach((st) -> {
+            st.accept(text);
+            System.out.println("new text: " + text);
+        });
     }
 
-    public int getPosition() {
-        return position;
+    public void addTextObserver(Consumer<String> observer) {
+        textObservers.add(observer);
     }
 
-    public void setPosition(int position) {
-        this.position = position;
+    public void removeTextObserver(Consumer<String> observer) {
+        textObservers.remove(observer);
+    }
+
+    public int getCaretPosition() {
+        return caretPosition;
+    }
+
+    public void setCaretPosition(int caretPosition) {
+        this.caretPosition = caretPosition;
     }
 }
