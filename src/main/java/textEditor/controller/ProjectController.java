d2e73ecf1a977abc749ea6755fb4b4c140bd67f5
mdjdrn1 <mdjdrn1@gmail.com>
2018-01-09 20:16:00 +0100

    Moved editProjectView creation to ProjectPopupViewFactory

--- src/main/java/textEditor/controller/ProjectController.java
+++ src/main/java/textEditor/controller/ProjectController.java
@@ -4,14 +4,9 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.geometry.Insets;
-import javafx.geometry.Orientation;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Font;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import textEditor.RMIClient;
@@ -118,45 +113,8 @@ public class ProjectController implements Initializable, UserInjectionTarget, Cl
 
         // TODO: Remove code duplication. Find better solution to avoid another fxml file and another controller.
         editButton.setOnAction(event -> {
-            final Stage popup = new Stage();
-            popup.initModality(Modality.APPLICATION_MODAL);
+            final Stage popup = ProjectPopupViewFactory.createEditProjectView();
 
-            VBox vbox = new VBox(10);
-            vbox.setPadding(new Insets(10));
-
-            Label newProjectLabel = new Label("Edit project");
-            newProjectLabel.setFont(new Font("System Bold", 20));
-
-            Label projectNameLabel = new Label("Project name");
-            projectNameLabel.setFont(new Font("System Bold", 12));
-            TextField projectNameField = new TextField("");
-
-            Label projectDescriptionLabel = new Label("Project description");
-            projectDescriptionLabel.setFont(new Font("System Bold", 12));
-            TextField projectDescriptionField = new TextField("");
-            projectDescriptionField.setPrefHeight(200);
-            projectDescriptionField.setAlignment(Pos.TOP_LEFT);
-
-            Label contributorsLabel = new Label("Contributors");
-            contributorsLabel.setFont(new Font("System Bold", 12));
-            TextField contributorsField = new TextField("");
-
-            Button addButton = new Button("Add");
-            Button cancelButton = new Button("Cancel");
-
-            HBox buttonBox = new HBox();
-            buttonBox.setSpacing(10);
-            buttonBox.setPadding(new Insets(10));
-            buttonBox.setAlignment(Pos.CENTER);
-            buttonBox.getChildren().addAll(addButton, cancelButton);
-
-            vbox.getChildren().addAll(newProjectLabel, new Separator(Orientation.HORIZONTAL),
-                    projectNameLabel, projectNameField, new Separator(Orientation.HORIZONTAL),
-                    projectDescriptionLabel, projectDescriptionField, new Separator(Orientation.HORIZONTAL),
-                    contributorsLabel, contributorsField,
-                    buttonBox);
-
-            popup.setScene(new Scene(vbox, 400, 500));
             popup.show();
         });
 
--- src/main/java/textEditor/controller/projectManagerPopups/ProjectPopupViewFactory.java
+++ src/main/java/textEditor/controller/projectManagerPopups/ProjectPopupViewFactory.java
@@ -11,6 +11,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class ProjectPopupViewFactory {
@@ -21,6 +22,50 @@ public class ProjectPopupViewFactory {
         vbox.setPadding(new Insets(10));
 
         Label newProjectLabel = new Label("New project");
+        newProjectLabel.setFont(new Font("System Bold", 20));
+
+        Label projectNameLabel = new Label("Project name");
+        projectNameLabel.setFont(new Font("System Bold", 12));
+        TextField projectNameField = new TextField("");
+
+        Label projectDescriptionLabel = new Label("Project description");
+        projectDescriptionLabel.setFont(new Font("System Bold", 12));
+        TextField projectDescriptionField = new TextField("");
+        projectDescriptionField.setPrefHeight(200);
+        projectDescriptionField.setAlignment(Pos.TOP_LEFT);
+
+        Label contributorsLabel = new Label("Contributors");
+        contributorsLabel.setFont(new Font("System Bold", 12));
+        TextField contributorsField = new TextField("");
+
+        Button addButton = new Button("Add");
+        Button cancelButton = new Button("Cancel");
+
+        HBox buttonBox = new HBox();
+        buttonBox.setSpacing(10);
+        buttonBox.setPadding(new Insets(10));
+        buttonBox.setAlignment(Pos.CENTER);
+        buttonBox.getChildren().addAll(addButton, cancelButton);
+
+        vbox.getChildren().addAll(newProjectLabel, new Separator(Orientation.HORIZONTAL),
+                projectNameLabel, projectNameField, new Separator(Orientation.HORIZONTAL),
+                projectDescriptionLabel, projectDescriptionField, new Separator(Orientation.HORIZONTAL),
+                contributorsLabel, contributorsField,
+                buttonBox);
+
+        view.setScene(new Scene(vbox, 400, 500));
+
+        return view;
+    }
+
+    public static Stage createEditProjectView() {
+        Stage view = new Stage();
+        view.initModality(Modality.APPLICATION_MODAL);
+
+        VBox vbox = new VBox(10);
+        vbox.setPadding(new Insets(10));
+
+        Label newProjectLabel = new Label("Edit project");
         newProjectLabel.setFont(new Font("System Bold", 20));
 
         Label projectNameLabel = new Label("Project name");
