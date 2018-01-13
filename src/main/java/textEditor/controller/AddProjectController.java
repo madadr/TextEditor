6ea6dfaac05284c0d39af46c5ba3b0648fecbd9e
Kamil Majerczyk <kamil.majerczyk@ericsson.com>
2018-01-13 12:17:20 +0100

    Added checking contributors from friendlist when creating and editing project

--- src/main/java/textEditor/controller/AddProjectController.java
+++ src/main/java/textEditor/controller/AddProjectController.java
@@ -3,8 +3,11 @@ package textEditor.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
+import org.controlsfx.control.CheckComboBox;
 import textEditor.RMIClient;
 import textEditor.model.DatabaseModel;
 import textEditor.view.WindowSwitcher;
@@ -13,16 +16,19 @@ import java.io.IOException;
 import java.net.URL;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.ResourceBundle;
 
 public class AddProjectController implements Initializable, ClientInjectionTarget, WindowSwitcherInjectionTarget, UserInjectionTarget {
     @FXML
-    public TextField projectNameField, contributorsField;
+    public TextField projectNameField;
+    @FXML
+    public CheckComboBox contributorsField;
     @FXML
     public TextArea projectDescriptionField;
+    @FXML
+    public Label information;
 
 
     private RMIClient rmiClient;
@@ -36,7 +42,9 @@ public class AddProjectController implements Initializable, ClientInjectionTarge
     public void initialize(URL location, ResourceBundle resources) {
         try {
             databaseModel = (DatabaseModel) rmiClient.getModel("DatabaseModel");
-            contributorsField.setText(user.getUsername());
+            contributorsField.getItems().add(user);
+            contributorsField.getItems().addAll(databaseModel.getFriends(user));
+            contributorsField.getCheckModel().check(user);
         } catch (RemoteException | NotBoundException e) {
             e.printStackTrace();
         }
@@ -59,13 +67,30 @@ public class AddProjectController implements Initializable, ClientInjectionTarge
 
     public void addButtonClicked(ActionEvent actionEvent) {
         try {
-            List<String> arrayList = Arrays.asList(contributorsField.getText().split(","));
-            Project project = new ProjectImpl(0, projectNameField.getText(), projectDescriptionField.getText(), Arrays.asList(contributorsField.getText().split(",")));
+            List contributors = Arrays.asList(contributorsField.getCheckModel().getCheckedItems().toArray());
+            if(!contributors.contains(user))
+            {
+                setInformation("You can't create a project without participating in it!");
+                return;
+            }
+            if(projectNameField.getText().isEmpty())
+            {
+                setInformation("You need to fill project name");
+                return;
+            }
+            Project project = new ProjectImpl(0, projectNameField.getText(), projectDescriptionField.getText(), contributors);
             databaseModel.addProject(project);
             windowSwitcher.loadWindow(WindowSwitcher.Window.PICK_PROJECT);
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private void setInformation(String text)
+    {
+        information.setVisible(true);
+        information.setTextFill(Color.RED);
+        information.setText(text);
     }
 
     public void cancelButtonClicked(ActionEvent actionEvent) {
