3c1a5eb1beb1f4199ebcafa31bd066d827bfb746
mdjdrn1 <mdjdrn1@gmail.com>
2018-01-14 14:34:43 +0100

    Updated threads

--- src/main/java/textEditor/model/EditorControllerObserver.java
+++ src/main/java/textEditor/model/EditorControllerObserver.java
@@ -1,7 +1,6 @@
 package textEditor.model;
 
 
-import javafx.application.Platform;
 import org.fxmisc.richtext.StyleClassedTextArea;
 import textEditor.model.interfaces.EditorModel;
 import textEditor.model.interfaces.RemoteObservable;
@@ -26,20 +25,24 @@ public class EditorControllerObserver implements Serializable, RemoteObserver {
 
     @Override
     public void update(RemoteObservable observable) throws RemoteException {
-        Platform.runLater(new UpdateTextWrapper(observable));
-        Platform.runLater(new UpdateStyleWrapper(observable));
+//        Platform.runLater(new UpdateTextWrapper(observable));
+        new Thread(new UpdateTextWrapper(observable)).start();
+//        Platform.runLater(new UpdateStyleWrapper(observable));
+        new Thread(new UpdateStyleWrapper(observable)).start();
     }
 
     @Override
     public synchronized void update(RemoteObservable observable, RemoteObservable.UpdateTarget target) throws RemoteException {
         if (target == RemoteObservable.UpdateTarget.ONLY_TEXT) {
             System.out.println("LOG LOG ");
-            Platform.runLater(new UpdateTextWrapper(observable));
+//            Platform.runLater(new UpdateTextWrapper(observable));
+            new Thread(new UpdateTextWrapper(observable)).start();
             System.out.println("LOG LOG 123213312312123");
         }
 
         if (target == RemoteObservable.UpdateTarget.ONLY_STYLE) {
-            Platform.runLater(new UpdateStyleWrapper(observable));
+            new Thread(new UpdateStyleWrapper(observable)).start();
+//            Platform.runLater(new UpdateStyleWrapper(observable));
         }
     }
 
@@ -81,14 +84,19 @@ public class EditorControllerObserver implements Serializable, RemoteObserver {
         private RemoteObservable observable;
 
         public UpdateTextWrapper(RemoteObservable observable) {
+            System.out.println("UpdateTextWrapper ctro");
             this.observable = observable;
         }
 
         @Override
         public void run() {
+            System.out.println("UpdateTextWrapper run");
             isUpdating.set(true);
+            System.out.println(textArea == null ? "\t\ttextarea is null " : "\t\ttextarea not null" );
             int oldCaretPosition = textArea.getCaretPosition();
+            System.out.println("UpdateTextWrapper getCaretPosition");
             String oldText = textArea.getText();
+            System.out.println("UpdateTextWrapper getText");
             try {
                 String newText = ((EditorModel) observable).getTextString();
                 System.out.println("newText " + newText);
@@ -115,8 +123,12 @@ public class EditorControllerObserver implements Serializable, RemoteObserver {
             StylesHolder newStyle = null;
             try {
                 newStyle = ((EditorModel) observable).getTextStyle();
+
+                System.out.println(newStyle.getStyleSpans() == null ? "TO TU JEST PROBLEM " : "TO NIE TU JEST PROBLEM ");
+
                 if (newStyle != null && newStyle.getStyleSpans() != null) {
-//                    System.out.println("not null");
+                    System.out.println("not null");
+                    System.out.println("\t\t\t newStyle" + newStyle);
                     updateStyleSpans(newStyle);
                     updateParagraphs(newStyle);
                 }
@@ -126,6 +138,17 @@ public class EditorControllerObserver implements Serializable, RemoteObserver {
         }
 
         private void updateParagraphs(StylesHolder newStyle) {
+            if(newStyle == null) {
+                System.out.println("INNA OOODUPA");
+            }
+
+            if(newStyle.getParagraphStyles() == null) {
+                System.out.println("INNA DUPA ");
+
+            } else {
+                System.out.println("newStyle.getParagraphStyles() " + newStyle.getParagraphStyles());
+            }
+
             List<List<String>> paragraphStyles = newStyle.getParagraphStyles();
             for (int i = 0; i < paragraphStyles.size(); ++i) {
                 textArea.setParagraphStyle(i, paragraphStyles.get(i));
@@ -133,6 +156,28 @@ public class EditorControllerObserver implements Serializable, RemoteObserver {
         }
 
         private void updateStyleSpans(StylesHolder newStyle) {
+            if(newStyle == null) {
+                System.out.println("OOODUPA");
+            }
+
+            if(textArea == null) {
+                System.out.println("textarea null");
+            } else {
+                System.out.println("textarea not null");
+            }
+
+            if(newStyle.getStyleSpans() == null) {
+                System.out.println("DUPA ");
+
+            } else {
+                System.out.println("newStyle.getStylesStart() " + newStyle.getStylesStart());
+            }
+
+            if(newStyle.getStyleSpans() == null) {
+                System.out.println("DUPA UDPAUDPAUPUADP UPDAP UD");
+            } else {
+                System.out.println("newStyle.getStyleSpans() " + newStyle.getStyleSpans());
+            }
             textArea.setStyleSpans(newStyle.getStylesStart(), newStyle.getStyleSpans());
         }
     }
